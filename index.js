596cbbd3a52e74343292ccc983d497d5ab3feb54
JimmyKimunyi <jimmykimunyi@gmail.com>
2023-07-11 12:27:18 +0300

    uibuild

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.static("dist"));
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error);
+  console.error(error);
   next(error);
   return;
 };
